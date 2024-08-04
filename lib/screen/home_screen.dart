6c9e9025bc9c3c98607bbb8650ba95bddaab8abd
aillagerbear <123285515+aillagerbear@users.noreply.github.com>
2024-08-04 19:54:57 +0900

    2024.08.04 - 더미테이터 삭제

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -29,9 +29,17 @@ class _HomeScreenState extends State<HomeScreen> {
     print('Selected day: $selectedDay');
 
     try {
+      final user = Supabase.instance.client.auth.currentUser;
+      if (user == null) {
+        throw 'User is not authenticated';
+      }
+
       final response = await Supabase.instance.client
           .from('selected_dates')
-          .insert({'date': selectedDay.toIso8601String()});
+          .insert({
+        'user_id': user.id,  // 유저 ID 추가
+        'date': selectedDay.toIso8601String()
+      });
 
       if (response.error != null) {
         // 에러 처리
@@ -72,106 +80,10 @@ class _HomeScreenState extends State<HomeScreen> {
               CircularProgressIndicator()  // 로딩 표시
             else
               Expanded(
-                child: ListView(
-                  padding: EdgeInsets.all(8.0),
-                  children: [
-                    ScheduleCard(
-                      startTime: '오전 10:00',
-                      endTime: '오전 12:00',
-                      estimatedPlayTime: '2시간',
-                      rule: 'D&D 5판',
-                      scenario: '파인들버의 잃어버린 광산',
-                      participants: '앨리스, 밥, 찰리',
-                      keeper: '데이브',
-                      players: '이브, 프랭크, 그레이스',
-                    ),
-                    ScheduleCard(
-                      startTime: '오후 2:00',
-                      endTime: '오후 4:00',
-                      estimatedPlayTime: '2시간',
-                      rule: '패스파인더',
-                      scenario: '룬 군주의 부활',
-                      participants: '헨리, 이안, 잭',
-                      keeper: '케이트',
-                      players: '레오, 미아, 니나',
-                    ),
-                  ],
-                ),
+                child: Container(),  // 더미 데이터 카드 제거
               ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class ScheduleCard extends StatelessWidget {
-  final String startTime;
-  final String endTime;
-  final String estimatedPlayTime;
-  final String rule;
-  final String scenario;
-  final String participants;
-  final String keeper;
-  final String players;
-
-  const ScheduleCard({
-    required this.startTime,
-    required this.endTime,
-    required this.estimatedPlayTime,
-    required this.rule,
-    required this.scenario,
-    required this.participants,
-    required this.keeper,
-    required this.players,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-      elevation: 2,
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildRow('시작 시간', startTime),
-            _buildRow('종료 예정 시간', endTime),
-            _buildRow('플레이 예상 시간', estimatedPlayTime),
-            _buildRow('룰', rule),
-            _buildRow('시나리오', scenario),
-            _buildRow('참가자', participants),
-            _buildRow('키퍼', keeper),
-            _buildRow('플레이어', players),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildRow(String title, String value) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Flexible(
-            child: Text(
-              value,
-              style: TextStyle(
-                color: Colors.grey[600],
-              ),
-              textAlign: TextAlign.end,
-            ),
-          ),
-        ],
       ),
     );
   }
