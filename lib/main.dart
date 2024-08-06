f79235ca88c3b8adae0eebcfc8c2ef2c42d1eacc
aillagerbear <123285515+aillagerbear@users.noreply.github.com>
2024-08-07 00:27:11 +0900

    2024.08.06 - vercel 용 수정5

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:calendar_trpg/screen/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  String supabaseUrl = '';
-  String supabaseAnonKey = '';
-
-  try {
-    // 로컬 환경에서 .env 파일 로드 시도
-    await dotenv.load(fileName: '.env');
-    supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
-    supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'] ?? '';
-  } catch (e) {
-    // .env 파일 로딩 실패 시 (주로 배포 환경에서) 기본값 사용
-    print('Failed to load .env file. Using environment variables.');
-  }
-
-  // .env에서 로드 실패 시 환경 변수에서 직접 로드
-  if (supabaseUrl.isEmpty) {
-    supabaseUrl = const String.fromEnvironment('SUPABASE_URL', defaultValue: '');
-  }
-  if (supabaseAnonKey.isEmpty) {
-    supabaseAnonKey = const String.fromEnvironment('SUPABASE_ANON_KEY', defaultValue: '');
-  }
+  const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
 
   if (supabaseUrl.isEmpty || supabaseAnonKey.isEmpty) {
-    throw Exception('Supabase URL or Anon Key not found. Please check your environment variables or .env file.');
+    throw Exception('Supabase URL or Anon Key not found. Please check your environment variables.');
   }
 
   await Supabase.initialize(
